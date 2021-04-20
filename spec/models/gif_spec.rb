9b89d4ca0a39c3e2abb8687414b10038af72d7eb
Mateus <mateusdeap@gmail.com>
2021-04-20 09:01:05 -0300

    Moves gifs from spec/models to spec/fixtures/files

--- spec/models/gif_spec.rb
+++ spec/models/gif_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Gif, type: :model do
   it "requires images smaller than 1 MB" do
     gif = Gif.new
-    gif.image.attach(io: File.open("./spec/models/the_blue_ribbon.gif"), filename: "the_blue_ribbon.gif", content_type: "image/gif")
+    gif.image.attach(io: File.open("./spec/fixtures/files/the_blue_ribbon.gif"), filename: "the_blue_ribbon.gif", content_type: "image/gif")
 
     expect(gif).to be_invalid
     expect(gif.errors.full_messages).to include("Image too big")
@@ -11,7 +11,7 @@ RSpec.describe Gif, type: :model do
 
   it "requires the image to be a gif" do
     gif = Gif.new
-    gif.image.attach(io: File.open("./spec/models/test.jpeg"), filename: "test.jpeg", content_type: "image/jpeg")
+    gif.image.attach(io: File.open("./spec/fixtures/files/test.jpeg"), filename: "test.jpeg", content_type: "image/jpeg")
 
     expect(gif).to be_invalid
     expect(gif.errors.full_messages).to include("Image wrong format")
@@ -19,7 +19,7 @@ RSpec.describe Gif, type: :model do
 
   it "succeeds validations if it is a gif" do
     gif = Gif.new
-    gif.image.attach(io: File.open("./spec/models/gif_2.gif"), filename: "gif_2.gif", content_type: "image/gif")
+    gif.image.attach(io: File.open("./spec/fixtures/files/gif_2.gif"), filename: "gif_2.gif", content_type: "image/gif")
 
     expect(gif).to be_valid
   end
